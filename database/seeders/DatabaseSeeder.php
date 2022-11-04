0c2ec7910ab677fe670e13c32ae70daa74a61462
Ashley Eatly <ashley.eatly@bcs.org>
2022-11-04 20:35:56 +0000

    lab 8???

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory(1)->create(['name'=>'Test','email'=>'test@test.com']);
+        User::factory(10)->create();
         $this->call([
             PeopleTableSeeder::class,
             CarsTableSeeder::class,
--- routes/web.php
+++ routes/web.php
@@ -16,51 +16,46 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $a = Person::all();
-    dump($a);
-    echo '</br>';
-    echo 'just give users';
-    // equivalent select * from users
-    $b = Person::find(1)->toSql();
-    echo $b;
-    echo '</br>';
-    $b = Person::all();
-    echo $b;
-    return "we are here";
-    // return view('welcome');
+//    $a = Person::all();
+//    dump($a);
+//    echo '</br>';
+//    echo 'just give users';
+//    // equivalent select * from users
+//    $b = Person::find(1)->toSql();
+//    echo $b;
+//    echo '</br>';
+//    $b = Person::all();
+//    echo $b;
+    // return "we are here";
+    return view('welcome');
 });
 
 Route::get('/people', [PersonController::class,'index'])
-    ->name('people.index');
+    ->name('people.index')
+->middleware('auth');
 
 Route::get('/people/create', [PersonController::class, 'create'])
-    ->name('people.create');
+    ->name('people.create')
+    ->middleware('auth');
 
 Route::post('/people', [PersonController::class, 'store'])
-    ->name('people.store');
+    ->name('people.store')
+    ->middleware('auth');
 
 Route::get('/people/{person}',[PersonController::class,'show'])
-    ->name('people.show');
+    ->name('people.show')
+    ->middleware('auth');
 
 Route::delete('/people/{person}',[PersonController::class,'destroy'])
-    ->name('people.destroy');
+    ->name('people.destroy')
+    ->middleware('auth');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
 
-Auth::routes();
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+require __DIR__.'/auth.php';
